d7aa59665d5fbd5ff9907df65dc8f998d40c8e32
iswahyudi <60358147+yudipd@users.noreply.github.com>
2021-08-17 12:16:11 +0700

    Add files via upload

---
+++ koneksi/koneksi.php
@@ -0,0 +1,12 @@
+<?php  
+
+error_reporting(0);
+
+//Database Configuration
+$db_name 	= "asmidb";
+$host		= "localhost";
+$username	= "root";
+$password	= "";
+
+//make connection to database
+$conn 		= mysqli_connect($host,$username,$password,$db_name) or die("Database connection error!");
