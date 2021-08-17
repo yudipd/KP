c33e01f4fcfe11e9eb140d27041784906594d402
iswahyudi <60358147+yudipd@users.noreply.github.com>
2021-08-17 12:15:54 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,29 @@
+**Alur Sistem**
+
+1) Proses Pendaftaran
+-melakukan pendaftaran melalui menu daftar
+-mengisi formulir pendaftaran sampai tuntas
+-Data yang diinputkan akan masuk kedalam tabel akun, pendaftaran, detail_pendaftaran
+-User akan di informasikan untuk melakukan login dengan email dan password untuk menyelesaikan pendaftaran
+-User harus mengupload Formulir akte kalahiran, kartu keluarga, Foto, dan foto ukuran 2r untuk menyelesaikan pendaftaran
+-data upload akan masuk ke dalam tabel pendaftaran
+-Setelah upload berhasil, maka syarat pendaftaran sudah selesai dilengkapi
+-tinggal menunggu konfirmasi dari admin
+-admin mengkonfirmasi pendaftaran user
+-status pendaftaran user akan berubah menjadi 1 di table pendaftaran (Pendaftaran sudah dikonfirmasi oleh admin)
+-Proses pendaftaran selesai
+
+
+0 - baru daftar (blom selesai melengkapi administrasi didashboard seperti upload akter, dan foto)
+1 - pendaftaran sudah dikonfirmasi oleh admin
+
+
+
+*ADMIN
+email		: 	admin@gmail.com
+password	:	12345678
+
+
+*USER
+email 		: 	a@gmail.com
+password 	:	12345678
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+	<link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Penerimaan Mahasiswa Baru</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name="viewport" content="width=device-width" />
+	<!-- Bootstrap core CSS     -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+	<!--  Material Dashboard CSS    -->
+	<link href="assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet" />
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+	<link href="assets/css/demo.css" rel="stylesheet" />
+	<!--     Fonts and icons     -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="../assets/css/material-dashboard.css">
+
+</head>
+
+<body>
+	<canvas id="canvas"></canvas>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-2 losdol">
+				<div class="card-background">
+
+					<div class=" col-md-6 col-lg-5 col-sm-12 student">
+						<center>
+							<a href="login.php">
+								<img src="assets/img/student.png" alt="Login">
+								<h5>LOGIN</h5>
+							</a>
+						</center>
+					</div>
+					
+					<div class="col-md-6 col-lg-5 col-sm-12 note">
+						<center>
+							<a href="daftar_akun.php">
+								<img src="assets/img/note.png" alt="Pendaftaran Siswa Baru">
+								<h5>DAFTAR</h5>
+							</a>
+						</center>
+					</div>
+					
+				</div>
+				<div class="col-md-6 col-lg-8 col-sm-12 note">
+				<a href="index.php" class="text-center new-account">Kembali </a>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+	
+	<script src="cuk1.js"></script>
+	
+</body>
+
+</html>
